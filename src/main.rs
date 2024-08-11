b7d985b79c4169445a0a5d5cbac935dcef00d356
zengolutil <zengolutil@gmail.com>
2024-08-11 16:18:27 -0300

    Horarios de Clases de Algebra-24

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+fn main() {
+
+
+    println!("BEGIN:VCALENDAR
+
+SUMMARY:Clase de Teoría de AGA para 1H8.
+RRULE:FREQ=WEEKLY;WKST=SU;UNTIL=20241129T190000Z;INTERVAL=1;BYDAY=FR
+UID:ZOOM99810449973
+TZID:America/Argentina/Buenos_Aires
+DESCRIPTION:Iván Martínez is inviting you to a scheduled Zoom meeting.\n\
+ nJoin 
+ Zoom Meeting\nhttps://utn.zoom.us/j/99810449973\n\nMeeting 
+ ID: 998 1044 9973\n\n
+LOCATION:https://utn.zoom.us/j/99810449973
+TIME: 15:00 TODOS LOS VIERNES.
+COMIENZA: 16 ago 2024 15:00 Buenos Aires, Georgetown
+16 ago 2024 15:00
+23 ago 2024 15:00
+30 ago 2024 15:00
+6 sept 2024 15:00
+13 sept 2024 15:00
+20 sept 2024 15:00
+27 sept 2024 15:00
+4 oct 2024 15:00
+11 oct 2024 15:00
+18 oct 2024 15:00
+25 oct 2024 15:00
+1 nov 2024 15:00
+8 nov 2024 15:00
+15 nov 2024 15:00
+22 nov 2024 15:00
+");
+}
